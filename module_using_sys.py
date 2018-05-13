3b9e8070dcd82bac081760859fb7c31419079e0a
teddyhong <rusdlsla@gmail.com>
2018-05-13 16:12:31 +0900

    study python module

---
+++ module_using_sys.py
@@ -0,0 +1,9 @@
+import sys
+print('The command line arguments are:') 
+for i in sys.argv:
+    print (i)
+
+print ('\n\nThe PYTHONPATH is', sys.path, '\n')
+
+# execute python module_using_sys.py we are arguments
+
