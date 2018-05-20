74e1f2eff1906bf750d3c982c0dd41f79274815a
teddyhong <rusdlsla@gmail.com>
2018-05-21 00:32:05 +0900

    추가 사항

---
+++ assert.py
@@ -0,0 +1,7 @@
+mylist = ['item']
+assert len(mylist) >= 1
+
+print mylist.pop()
+
+assert len(mylist) >= 1
+
---
+++ more_tuple.py
@@ -0,0 +1,18 @@
+def get_error_details():
+    return (2, 'details')
+
+errnum, errstr = get_error_details()
+
+print errnum
+print errstr
+
+
+a = 5; b = 8
+print a
+print b
+
+a, b = b, a
+
+print a
+print b
+
