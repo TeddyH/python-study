9cb5ba79fb92d468903405306d3ed174a5692a91
TeddyH <rusdlsla@gmail.com>
2019-01-07 09:52:59 +0900

    복소수 활용 예 추가

--- calc.py
+++ calc.py
@@ -6,3 +6,16 @@ b = 20
 c = a + b
 
 print(c)
+
+a = 1 + 2j
+# 복소수의 실수 부분을 리턴한다.
+print(a.real)
+
+# 복소수의 허수 부분을 리턴한다.
+print(a.imag)
+
+# 복소수의 켤레복소수를 리턴한다.
+print(a.conjugate())
+
+# 복소수의 절대값을 리턴한다.
+print(abs(a))
